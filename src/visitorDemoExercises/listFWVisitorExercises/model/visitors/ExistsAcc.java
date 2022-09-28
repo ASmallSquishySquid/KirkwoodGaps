249b619b621a573e78ea225d6ec6104cfb3eaffa
Phoebe Scaccia <phoebescaccia@gmail.com>
2022-09-28 16:23:59 -0500

    Add the algos for all accumulations

---
+++ src/visitorDemoExercises/listFWVisitorExercises/model/visitors/ExistsAcc.java
@@ -0,0 +1,33 @@
+package visitorDemoExercises.listFWVisitorExercises.model.visitors;
+
+import provided.listFW.visitors.AAccumulator;
+
+/**
+ * TODO
+ * 
+ * @author Phoebe Scaccia
+ *
+ */
+public class ExistsAcc extends AAccumulator {
+
+	/**
+	 * The value to search for.
+	 */
+	private int search;
+	
+	/**
+	 * Constructor for a new ExistsAcc.
+	 * 
+	 * @param value a String containing the value to search for
+	 */
+	public ExistsAcc(Object value) {
+		super(false);
+		this.search = Integer.parseInt((String) value);
+	}
+
+	@Override
+	public void accumulate(Object x) {
+		this.value = (boolean) this.value | (int) x == search;
+	}
+
+}
