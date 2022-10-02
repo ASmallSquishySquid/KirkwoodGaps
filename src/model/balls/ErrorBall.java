e65fb79c2f268e3be59721179e207c80d060451b
Phoebe Scaccia <phoebescaccia@gmail.com>
2022-10-02 12:04:37 -0500

    Update error ball

--- src/model/balls/ErrorBall.java
+++ src/model/balls/ErrorBall.java
@@ -6,14 +6,41 @@ import java.awt.Point;
 
 import model.adapters.IModel2BallAdapter;
 import model.visitors.algos.AConfigBallAlgo;
+import model.visitors.algos.ErrorConfigBallAlgo;
 import provided.ballworld.extVisitors.IBallHostID;
+import provided.ballworld.extVisitors.impl.BallHostIDFactory;
 
+/**
+ * A ball that handles errors.
+ * 
+ * @author Phoebe Scaccia
+ */
 public class ErrorBall extends ABall {
 
-	public ErrorBall(IBallHostID id, Point p, int r, Point v, Color c, Component container,
+	/**
+	 * For serialization.
+	 */
+	private static final long serialVersionUID = -8935415963329780965L;
+	
+	/**
+	 *  The identifying host ID for this class.
+	 */
+	public static final IBallHostID id = BallHostIDFactory.Singleton.makeID(ErrorBall.class.getName());
+	
+	/**
+	 * Constructor for a new ErrorBall.
+	 * 
+	 * @param p the position.
+	 * @param r the radius.
+	 * @param v the velocity.
+	 * @param c the color.
+	 * @param container the container.
+	 * @param installAlgo the algo to complete the installation of strategies and any other desired operations
+	 * @param modelAdapter The adapter to the model this ball is used in
+	 */
+	public ErrorBall(Point p, int r, Point v, Color c, Component container, 
 			AConfigBallAlgo installAlgo, IModel2BallAdapter modelAdapter) {
-		super(id, p, r, v, c, container, installAlgo, modelAdapter);
-		// TODO Auto-generated constructor stub
+		super(id, p, r, v, c, container, new ErrorConfigBallAlgo(), modelAdapter);
 	}
 
 }
