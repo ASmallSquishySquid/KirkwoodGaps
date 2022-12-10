0483dcec77e96b66c1955f186bdcc4960dd39660
Phoebe Scaccia <phoebescaccia@gmail.com>
2022-12-10 15:08:05 -0600

    Add initial velocity calculation

--- src/model/Constants.java
+++ src/model/Constants.java
@@ -2,6 +2,8 @@ package model;
 
 import java.awt.geom.Point2D;
 
+import provided.utils.valueGenerator.impl.VectorUtil;
+
 /**
  * Holds the constants.
  * 
@@ -9,12 +11,41 @@ import java.awt.geom.Point2D;
  */
 public class Constants {
 	/**
+	 * The mass of the sun.
+	 */
+	public static double sunMass = 1.9885e30;
+	
+	/**
 	 * Conversion value for km to pixels for GUI display.
 	 */
 	public static double kmToPixels = 1e4;
 	
 	/**
+	 * The gravitational constant.
+	 */
+	public static double gravitationalConstant = 6.673e-11;
+	
+	/**
 	 * The position of the sun in the GUI.
 	 */
 	public static Point2D.Double sunPosition = new Point2D.Double();
+	
+	/**
+	 * Calculates the starting velocity.
+	 *
+	 * @param distance the distance from the sun in km
+	 * @param angle the angle around the sun in radians
+	 * @return the starting velocity as a Point2D.Double
+	 */
+	public static Point2D.Double calculateVelocity(double distance, double angle) {
+		
+		double underSquare = Constants.gravitationalConstant * Constants.sunMass / distance;
+		
+		Point2D.Double velocity = new Point2D.Double(Math.sqrt(underSquare), 0);
+		
+		VectorUtil.Singleton.rotate(velocity, angle);
+		
+		return velocity;
+		
+	}
 }
